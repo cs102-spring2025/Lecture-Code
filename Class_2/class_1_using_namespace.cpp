e09c307c945f0f173ca31972e9b1a179e859b466
kdoke <31821416+kdoke@users.noreply.github.com>
2025-01-24 10:07:52 -0500

    Lecture code 1_24_25

---
+++ Class_2/class_1_using_namespace.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+/**
+ * Using namespace std is considered bad practice.
+ * Alternative to this statement is to specify the namespace
+ * to which the identifier belongs to (i.e. std::cout).
+ * It imports all the symbols inside std, which is considered bad
+ * practice especially in header files --> increases the risk
+ * of collisions with your own symbols.
+ */
+int main() {
+   string test = "";
+   cout << "Please enter a string: ";
+   cin >> test; // cin will only read up until the whitespace - more to come!
+   cout << test << endl;
+  
+  return 0;
+}
+
+// To run, execute the following commands:
+// g++ class_1_using_namespace.cpp -o myprogram2
+// ./myprogram2
