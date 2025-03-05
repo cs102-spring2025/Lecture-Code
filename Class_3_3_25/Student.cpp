1ebf56cfb9f536b070eaa88c44c3424246f3b9cd
kdoke <31821416+kdoke@users.noreply.github.com>
2025-03-05 09:59:55 -0500

    Update Student.cpp

--- Class_3_3_25/Student.cpp
+++ Class_3_3_25/Student.cpp
@@ -52,9 +52,9 @@ class Student {
 
     int main() {
 
-      Student student1 = Student("Alice Brown", 3.5, 88.0, 91.0);
+      Student student1("Alice Brown", 3.5, 88.0, 91.0);
       student1.write_output();
-      Student student2 = Student("John Smith", 4.0);
+      Student student2("John Smith", 4.0);
       student2.write_output();
       student2.set_cs_grade(99.0);
       student2.write_output();
