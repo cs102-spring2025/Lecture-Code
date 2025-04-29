ab46e814f71496240022a87317283ac86d228c1b
kdoke <31821416+kdoke@users.noreply.github.com>
2025-04-29 10:59:17 -0400

    Update dollars.cpp

--- Class_4_28_25/dollars.cpp
+++ Class_4_28_25/dollars.cpp
@@ -19,26 +19,6 @@ Dollars::Dollars(int dollars, int cents) {
     _cents = cents;
 }
 
-
-Dollars Dollars::operator*(const Dollars &other) {
-    
-//int amount = (_dollars * other._dollars) + (_dollars *other._cents)  
-//            +(other._dollars * _cents) + ( other._cents * _cents);
-
- //   int dollars = amount/100;
- //   int cents = amount%100;
-
-    int cents = _cents * other._cents % 100;
-    int dollars = (_dollars * other._cents) + ((_cents * other._cents)/100);
-
-    int cents2 = (other._dollars * _cents);
-    int dollars2 = other._dollars * _dollars;
-    cout << cents2 << dollars2 << endl;
-
-    return Dollars(dollars, cents);
-
-}
-
 Dollars Dollars::operator*(const int &factor) {
     int cents = _cents * factor % 100;
    // cout << _cents << _cents*factor << endl;
@@ -46,8 +26,6 @@ Dollars Dollars::operator*(const int &factor) {
     return Dollars(dollars, cents);
 }
     
-
-
 Dollars Dollars::operator+(const Dollars &other) {
     int cents = _cents + other._cents;
     int dollars = _dollars + other._dollars + (cents/100);
@@ -67,17 +45,16 @@ ostream& operator<<(ostream &out, const Dollars &d) {
 int main() {
     Dollars change(1, 2);
     Dollars a_bit_more(1, 5);
-    //cout << change << endl;
+    cout << change << endl;
 
     Dollars money = change * a_bit_more;
     cout << money << endl; 
 
+    Dollars mon = change * 5;
+    cout << mon << endl;
+    Dollars money = change + a_bit_more;
+    cout << money << endl;
 
-    //Dollars mon = change * 5;
-   // cout << mon << endl;
-   // Dollars money = change + a_bit_more;
-   // cout << money << endl;
-
-   // cout << money.get_dollars() << " dollars and " << money.get_cents() << " cents" << endl;
+    cout << money.get_dollars() << " dollars and " << money.get_cents() << " cents" << endl;
     
 }
